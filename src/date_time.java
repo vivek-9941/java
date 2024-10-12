2c17ba084142355abedc17b1d458d4703fd8202d
Vivek Mane <vivekmane9860@gmail.com>
2024-10-12 11:45:47 +0530

    date and time in java

---
+++ src/date_time.java
@@ -0,0 +1,16 @@
+import java.util.Date;
+
+
+public class date_time {
+    public static void main(String[] args) {
+        Date d = new Date();
+//prints ddate;
+        System.out.println(d);
+        //prints time
+        System.out.println(d.getTime());
+
+
+
+
+    }
+}
